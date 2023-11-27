b1f07c7e73df23fefd9a2bf7b2859a36584ade39
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2023-11-27 23:09:12 +0530

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,28 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github.css';
 
 export default function Home() {
-  const highlightedCode = hljs.highlight('const str = "this string"', {
+  const [value, setValue] = useState('');
+
+  const highlightedCode = hljs.highlight(value, {
     language: 'javascript',
   }).value;
-
-  console.log(highlightedCode);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1 className="text-3xl">Beautify your code snippets</h1>
-        {/* <textarea
+        <textarea
           name=""
           id=""
           cols={30}
           rows={10}
           className="code whitespace-pre"
-        ></textarea> */}
-        <div className="code"> const str = this</div>
+          onChange={(e) => setValue(e.target.value)}
+        ></textarea>
 
         <div
           className="text-4xl"
